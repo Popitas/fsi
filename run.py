f7df4430fbb929b4885487f1933124cfbee04089
Jorge Báez <Popitas@users.noreply.github.com>
2016-02-18 14:26:09 +0000

    Update run.py

--- run.py
+++ run.py
@@ -1,5 +1,10 @@
 import search
 
-mi_problema = search.GraphProblem('A', 'B', search.romania)
+mi_problema = search.RomaniaGPS('A', 'F', search.romania)
+
+print "Breadth search:"
 print search.breadth_first_graph_search(mi_problema)
+
+print "\nDepth search:"
+print search.depth_first_graph_search(mi_problema)
 
